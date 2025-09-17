6f3ae03ed2bece19dde53f954cd1933cd5d04885
Arthur <arthurkohl0@gmail.com>
2025-09-17 09:50:25 -0300

    commit inicial

---
+++ src/test/java/com/uninter/vidaplus/VidaplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uninter.vidaplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidaplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
