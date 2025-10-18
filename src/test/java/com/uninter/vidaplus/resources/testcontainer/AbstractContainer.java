35ddb3e4d6cab7496df701f12f4ce7efbcb73349
Arthur <arthurkohl0@gmail.com>
2025-10-18 13:38:44 -0300

    VP-1 - processo para truncar tabelas afim de evitar dependencia entre testes

--- src/test/java/com/uninter/vidaplus/resources/testcontainer/AbstractContainer.java
+++ src/test/java/com/uninter/vidaplus/resources/testcontainer/AbstractContainer.java
@@ -1,8 +1,13 @@
 package com.uninter.vidaplus.resources.testcontainer;
 
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
+
+import java.util.List;
 
 public abstract class AbstractContainer {
 
@@ -17,5 +22,24 @@ public abstract class AbstractContainer {
         registry.add("spring.datasource.url", genericContainer::getJdbcUrl);
         registry.add("spring.datasource.username", genericContainer::getUsername);
         registry.add("spring.datasource.password", genericContainer::getPassword);
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @AfterEach
+    public void truncateTables() {
+        List<String> tables = jdbcTemplate.queryForList(
+                "SELECT table_name FROM information_schema.tables " +
+                        "WHERE table_schema = DATABASE() " +
+                        "AND table_name != 'flyway_schema_history'",
+                String.class
+        );
+
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
+
+        tables.forEach(table -> jdbcTemplate.execute("TRUNCATE TABLE " + table));
+
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
     }
 }
