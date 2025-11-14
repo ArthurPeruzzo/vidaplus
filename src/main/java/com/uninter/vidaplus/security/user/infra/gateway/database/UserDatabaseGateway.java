973624930fe8d7956dfe4547ab63c7925c340473
Arthur <arthurkohl0@gmail.com>
2025-11-14 19:06:40 -0300

    VP-2 - remocao de mapper

--- src/main/java/com/uninter/vidaplus/security/user/infra/gateway/database/UserDatabaseGateway.java
+++ src/main/java/com/uninter/vidaplus/security/user/infra/gateway/database/UserDatabaseGateway.java
@@ -1,11 +1,12 @@
 package com.uninter.vidaplus.security.user.infra.gateway.database;
 
+import com.uninter.vidaplus.security.user.core.domain.Email;
 import com.uninter.vidaplus.security.user.core.domain.Role;
 import com.uninter.vidaplus.security.user.core.domain.RoleEnum;
 import com.uninter.vidaplus.security.user.core.domain.User;
+import com.uninter.vidaplus.security.user.core.domain.password.PasswordHash;
 import com.uninter.vidaplus.security.user.core.exception.ErrorAccessDatabaseException;
 import com.uninter.vidaplus.security.user.core.gateway.UserGateway;
-import com.uninter.vidaplus.security.user.core.gateway.mapper.UserMapper;
 import com.uninter.vidaplus.security.user.infra.entity.RoleEntity;
 import com.uninter.vidaplus.security.user.infra.entity.UserEntity;
 import com.uninter.vidaplus.security.user.infra.repository.RoleRepository;
@@ -23,14 +24,18 @@ import java.util.Optional;
 public class UserDatabaseGateway implements UserGateway {
 
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
     private final RoleRepository roleRepository;
 
     @Override
     public Optional<User> findByEmail(String email) {
         try {
             return userRepository.findByEmail(email)
-                    .map(userMapper::entityToDomain);
+                    .map(entity -> new User(
+                            entity.getId(),
+                            new Email(entity.getEmail()),
+                            new PasswordHash(entity.getPassword()),
+                            entity.getRoles().stream().map(roleEntity -> new Role(roleEntity.getId(), roleEntity.getName()))
+                    .toList()));
         } catch (Exception e) {
             log.error("Erro ao buscar usuario por email: {}", email, e);
             throw new ErrorAccessDatabaseException();
@@ -43,11 +48,16 @@ public class UserDatabaseGateway implements UserGateway {
             List<RoleEnum> roleEnums = user.getRoles().stream().map(Role::getName).toList();
             List<RoleEntity> roles = roleRepository.findByNameIn(roleEnums);
 
-            UserEntity userEntity = userMapper.domainToEntity(user);
+            UserEntity userEntity = new UserEntity(user.getEmail().value(), user.getPassword().getValue(),
+                    user.getRoles().stream().map(role -> new RoleEntity(role.getName())).toList());
             userEntity.setRoles(roles);
 
-            UserEntity userSaved = userRepository.save(userEntity);
-            return userMapper.entityToDomain(userSaved);
+            UserEntity entity = userRepository.save(userEntity);
+            return new User(
+                    entity.getId(),
+                    new Email(entity.getEmail()),
+                    new PasswordHash(entity.getPassword()),
+                    entity.getRoles().stream().map(roleEntity -> new Role(roleEntity.getId(), roleEntity.getName())).toList());
         } catch (Exception e) {
             log.error("Erro ao salvar usuario", e);
             throw new ErrorAccessDatabaseException();
