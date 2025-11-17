9441de6cd2df5b6c30ab7e9bf7aa633eaaf4f593
Arthur <arthurkohl0@gmail.com>
2025-11-17 18:57:58 -0300

    VP-2 - ajuste para record

--- src/main/java/com/uninter/vidaplus/appointment/infra/controller/json/response/AppointmentResponseJson.java
+++ src/main/java/com/uninter/vidaplus/appointment/infra/controller/json/response/AppointmentResponseJson.java
@@ -3,32 +3,24 @@ package com.uninter.vidaplus.appointment.infra.controller.json.response;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.uninter.vidaplus.appointment.core.domain.AppointmentStatus;
 import com.uninter.vidaplus.appointment.core.domain.AppointmentType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-@Getter
-@AllArgsConstructor
-public class AppointmentResponseJson {
+public record AppointmentResponseJson(Long id,
+                                      @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy") LocalDate appointmentDay,
+                                      DayOfWeek dayOfWeek,
+                                      LocalTime startTime,
+                                      LocalTime endTime,
+                                      @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
+                                      LocalDateTime dateCreated,
+                                      AppointmentPersonaResponseJson healthcareProfessional,
+                                      AppointmentPersonaResponseJson patient,
+                                      AppointmentHealthcareFacilityResponseJson healthcareFacility,
+                                      AppointmentStatus status,
+                                      AppointmentType type) {
 
-    private final Long id;
-    private final LocalDate appointmentDay;
-    private final DayOfWeek dayOfWeek;
-    private final LocalTime startTime;
-    private final LocalTime endTime;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm")
-    private final LocalDateTime dateCreated;
-
-    private final AppointmentPersonaResponseJson healthcareProfessional;
-    private final AppointmentPersonaResponseJson patient;
-    private final AppointmentHealthcareFacilityResponseJson healthcareFacility;
-
-    private final AppointmentStatus status;
-    private final AppointmentType type;
 }
 
