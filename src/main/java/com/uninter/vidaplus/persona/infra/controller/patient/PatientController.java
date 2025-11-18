025f7948bb7e749192790a6d588d9202e49e31ec
Arthur <arthurkohl0@gmail.com>
2025-11-18 19:15:24 -0300

    VP-3 - documentacao endpoints pacientes

--- src/main/java/com/uninter/vidaplus/persona/infra/controller/patient/PatientController.java
+++ src/main/java/com/uninter/vidaplus/persona/infra/controller/patient/PatientController.java
@@ -3,6 +3,11 @@ package com.uninter.vidaplus.persona.infra.controller.patient;
 import com.uninter.vidaplus.persona.core.usecase.patient.CreatePatientUseCase;
 import com.uninter.vidaplus.persona.infra.controller.patient.dto.PatientCreateDto;
 import com.uninter.vidaplus.persona.infra.controller.patient.json.request.PatientCreateRequestJson;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,17 +20,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/patients")
+@Tag(name = "Pacientes")
 public class PatientController {
 
     private final CreatePatientUseCase createPatientUseCase;
 
+    @Operation(
+            summary = "Criar um paciente",
+            description = "Cria uma novo paciente. Somente um administrador pode criar um paciente"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Paciente criado com sucesso", content = { @Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "409", description = "O email utilizado para a criação já está em uso", content = { @Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "401", description = "Não autenticado", content = { @Content(mediaType = "application/json")}),
+            @ApiResponse(responseCode = "403", description = "Acesso negado", content = { @Content(mediaType = "application/json")})
+    })
     @PostMapping
     public ResponseEntity<HttpStatus> create(@RequestBody @Valid PatientCreateRequestJson requestJson) {
         PatientCreateDto dto = new PatientCreateDto(
-                requestJson.name(), requestJson.lastName(),
-                requestJson.email(),requestJson.sex(),
-                requestJson.healthcareFacilityId(),
-                requestJson.password());
+                requestJson.getName(), requestJson.getLastName(),
+                requestJson.getEmail(),requestJson.getSex(),
+                requestJson.getHealthcareFacilityId(),
+                requestJson.getPassword());
 
         createPatientUseCase.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
--- src/main/java/com/uninter/vidaplus/persona/infra/controller/patient/json/request/PatientCreateRequestJson.java
+++ src/main/java/com/uninter/vidaplus/persona/infra/controller/patient/json/request/PatientCreateRequestJson.java
@@ -1,19 +1,56 @@
 package com.uninter.vidaplus.persona.infra.controller.patient.json.request;
 
 import com.uninter.vidaplus.persona.core.domain.enums.SexEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-public record PatientCreateRequestJson (
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class PatientCreateRequestJson {
+        @Schema(
+                description = "Nome do paciente",
+                example = "Leonardo"
+        )
         @NotBlank(message = "O nome deve ser preenchido")
-        String name,
+        private String name;
+
+        @Schema(
+                description = "Sobrenome do paciente",
+                example = "Souza"
+        )
         @NotBlank(message = "O sobrenome deve ser preenchido")
-        String lastName,
+        private String lastName;
+
+        @Schema(
+                description = "Email do paciente",
+                example = "exemplo@exemplo.com"
+        )
         @NotBlank(message = "O email deve ser preenchido")
-        String email,
+        private String email;
+
+        @Schema(
+                description = "Sexo do paciente",
+                example = "MALE"
+        )
         @NotNull(message = "O sexo deve ser informado (FEMALE/MALE)")
-        SexEnum sex,
+        private SexEnum sex;
+
+        @Schema(
+                description = "Unidade hospitalar que o paciente será vinculado",
+                example = "1"
+        )
         @NotNull(message = "A unidade hospitalar deve ser informada")
-        Long healthcareFacilityId,
+        private Long healthcareFacilityId;
+
+        @Schema(
+                description = "A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais",
+                example = "VidaPlus2025!@#"
+        )
         @NotBlank(message = "A senha deve ser preenchida")
-        String password){}
+        private String password;
+}
